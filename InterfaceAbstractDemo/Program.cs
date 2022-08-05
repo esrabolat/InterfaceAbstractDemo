daad11a11da363bdc34f567ff8a1bfe9dea4c645
Esra <Esra@DESKTOP-12HFN4C>
2022-08-05 11:42:30 +0300

    Add project files.

---
+++ InterfaceAbstractDemo/Program.cs
@@ -0,0 +1,22 @@
+﻿using InterfaceAbstractDemo.Abstract;
+using InterfaceAbstractDemo.Concrete;
+using InterfaceAbstractDemo.Entities;
+using System;
+
+namespace InterfaceAbstractDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BaseCustomerManager customerManager = new NeroCustomerManager();
+            customerManager.Save(new Customer
+            {
+                DateOfBirth = new DateTime(1987, 1, 6),
+                FirstName = "Engin",
+                LastName = "Demiroğ",
+                NationalityId = "28285911040"
+            });
+        }
+    }
+}
